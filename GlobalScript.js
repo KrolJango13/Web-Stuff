f7e6f24fef602e4bd612c3e67e97da6d03999b52
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-05 16:06:47 -0500

    Added documentation

--- GlobalScript.js
+++ GlobalScript.js
@@ -9,7 +9,10 @@ function applyLink(id,url){
     byID(id).href = url.replaceAll(" ","%20");
     byID(id).target = "_blank";
 }
+// Create HTML table row (<tr>) element from an array
 const rowFromArray = (...cells) => `<tr><td>${cells.join("</td><td>")}</td></tr>`;
+
+// Boolean logic gates
 const Logic = {
     xor: (a, b) => (a || b) && !(a && b),
     xnor: (a, b) => !(a || b) || (a && b)
@@ -17,27 +20,38 @@ const Logic = {
 
 const query = (selector,element = document) => element.querySelector(selector)
 const queryAll = (selector,element = document) => element.querySelectorAll(selector)
+
 const JMath = {
-    coordPair: (x, y) => {return {x: x, y: y}},
+    // Return a coordinate pair of x and y
+    coordPair: (x, y) => {return {x: x, y: y, 0: x, 1: y}},
+    // Return the slope between
     slope: (x1,y1,x2,y2) => (y1 - y2) / (x1 - x2),
+    slope2: (a, b) => JMath.slope(a[0],a[1],b[0],b[1]),
+    // Return the angle of a line between 2 points
     vector: (x1,y1,x2,y2) => (Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI + 360) % 360,
-    vector2: (a, b) => JMath.vector(a.x || a[0], a.y || a[1], b.x || b[0], b.y || b[1]),
+    vector2: (a, b) => JMath.vector(a[0],a[1], b[0], b[1]),
+    // Advanced square root that supports imaginary numbers
     sqrt:(num) => {return {
         imaginary: num < 0,
         value: Math.sqrt(Math.abs(num)),
         toString: () => `${JMath.sqrt(num).value}${num < 0 ? "i" : ""}`
     }},
+    // Equivalent to the python range() function
     range:(start = 0, end, step = 1) => new Array(Math.ceil((end - start) / step)).fill(start).map((x,i) => x + (step*i)),
     range0: (end,step = 1) => JMath.range(0,end,step),
     factorial: (num) => JMath.range(1,num + 1).reduce((x,y) => x*=y),
+    // Get the factors of a number as an object
     factors(num){
         var facs = {}
         JMath.range(1,num).filter(x => num % x == 0).forEach(x => {if(!JObjects.hasKeyOrValue(facs,x)){facs[x] = num / x}})
         return facs
     },
     isPrime: (num) => Object.keys(JMath.factors(num)).length == 1,
+    // Get the greatest common factor of a set of numbers
     gcf: (...nums) => nums.reduce(function gcd(x,y){return x ? gcd(y % x,x) : y}),
+    // Get the least common multiple of a set of numbers
     lcm: (...nums) => nums.reduce((x,y) => (x*y) / JMath.gcf(x,y)),
+    // Advanced pi
     pi:{
         str: "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679",
         nums: () => [Math.PI].concat(JBigInt.toIntArray(2384626433832795028841971693993751058209749445923078164062862089986280348253421170679n))
@@ -45,6 +59,7 @@ const JMath = {
 }
 class JArray extends Array {
     array = () => Array.from(this)
+    // Split an array into chunks of a set size
     split(size){
         var arr = []
         while(this.length % size)this.push(null)
@@ -58,7 +73,9 @@ class JArray extends Array {
     static equals = (array, other) => new JArray(...array).equals(other)
 }
 const JSVG = {
+    // Create an SVG element
     createSVG: (tag = "svg") => document.createElementNS("http://www.w3.org/2000/svg", tag),
+    // Make an SVG polygon from an array of points and a color (if specified)
     createPolygon(points,color = "#00f"){
         var poly = JSVG.createSVG("polygon")
         points.forEach(coords => {
@@ -70,6 +87,7 @@ const JSVG = {
         poly.style.fill = color
         return poly
     },
+    // Move each point of a polygon, effectively moving the whole shape
     movePolygon(polygon,x = 0, y = 0){
         for(var point of polygon.points){
             point.x += x
@@ -77,6 +95,7 @@ const JSVG = {
         }
     },
     rotate: (element,degrees) => element.setAttribute("transform",`rotate(${degrees} ${element.x.baseVal.value} ${element.y.baseVal.value})`),
+    // Rotate an element to face a certain position
     pointTo: (element, x, y) => JSVG.rotate(element,JMath.vector(element.x.baseVal.value,element.y.baseVal.value,x,y)),
     shapes: {
         rect(x, y, width, height, color = "#00f"){
@@ -117,6 +136,7 @@ const JSVG = {
 const JXML = {
     parse: (string) => new DOMParser().parseFromString(string,"text/xml").children[0],
     byTag: (doc,tagName) => Array.from(doc.getElementsByTagName(tagName)),
+    // Convert an xml (or html) element to json
     toJSON(xml){
         var json = {
             type: xml.tagName,
@@ -137,6 +157,7 @@ const JXML = {
         Array.from(xml.children).forEach(x => json.children.push(JXML.toJSON(x)));
         return json;
     },
+    // Create an xml tag
     makeTag(name,attributes = {},classes = [],innerXML = ""){
         var tag = `<${name} `;
         Object.getOwnPropertyNames(attributes ? attributes : {}).forEach(x => tag += `${x}="${attributes[x]}" `);
@@ -150,4 +171,25 @@ const JXML = {
         html.getAttributeNames().forEach(attr => obj[attr] = html[attr])
         return obj;
     }
+}
+// Create a file input and append it to the document
+function genFileIn(){
+    var fileIn = document.createElement("input");
+    fileIn.type = "file";
+    fileIn.id = "fileIn";
+    document.body.append(fileIn);
+}
+// Get the users webcam, mic, or both and display it in a <video> element
+function getAV(videoElement,useVideo = true, useAudio = false){
+    navigator.getUserMedia({
+        audio: useAudio,
+        video: useVideo
+    },(v) => {
+        videoElement.srcObject = v;
+        videoElement.play();
+    },console.error);
+}
+const JRandom = {
+    // Return a random integer between 0 and the upper bound
+    int: (bound = 1000) => Math.floor(Math.random() * bound)
 }
