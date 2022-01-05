e5884be097ca9d50b3579447688fc5d95f2a64fd
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-05 13:18:56 -0500

    Update JMath

--- GlobalScript.js
+++ GlobalScript.js
@@ -21,7 +21,27 @@ const JMath = {
     coordPair: (x, y) => {return {x: x, y: y}},
     slope: (x1,y1,x2,y2) => (y1 - y2) / (x1 - x2),
     vector: (x1,y1,x2,y2) => (Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI + 360) % 360,
-    vector2: (a, b) => JMath.vector(a.x || a[0], a.y || a[1], b.x || b[0], b.y || b[1])
+    vector2: (a, b) => JMath.vector(a.x || a[0], a.y || a[1], b.x || b[0], b.y || b[1]),
+    sqrt:(num) => {return {
+        imaginary: num < 0,
+        value: Math.sqrt(Math.abs(num)),
+        toString: () => `${JMath.sqrt(num).value}${num < 0 ? "i" : ""}`
+    }},
+    range:(start = 0, end, step = 1) => new Array(Math.ceil((end - start) / step)).fill(start).map((x,i) => x + (step*i)),
+    range0: (end,step = 1) => JMath.range(0,end,step),
+    factorial: (num) => JMath.range(1,num + 1).reduce((x,y) => x*=y),
+    factors(num){
+        var facs = {}
+        JMath.range(1,num).filter(x => num % x == 0).forEach(x => {if(!JObjects.hasKeyOrValue(facs,x)){facs[x] = num / x}})
+        return facs
+    },
+    isPrime: (num) => Object.keys(JMath.factors(num)).length == 1,
+    gcf: (...nums) => nums.reduce(function gcd(x,y){return x ? gcd(y % x,x) : y}),
+    lcm: (...nums) => nums.reduce((x,y) => (x*y) / JMath.gcf(x,y)),
+    pi:{
+        str: "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679",
+        nums: () => [Math.PI].concat(JBigInt.toIntArray(2384626433832795028841971693993751058209749445923078164062862089986280348253421170679n))
+    }
 }
 class JArray extends Array {
     array = () => Array.from(this)
