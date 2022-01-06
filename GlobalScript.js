d67fcd629db1baa1239be3d80b9470c6423e992f
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-06 15:35:37 -0500

    Move SVG stuff to its own file

--- GlobalScript.js
+++ GlobalScript.js
@@ -38,67 +38,6 @@ class JArray extends Array {
     static split = (array, size) => new JArray(...array).split(size)
     static equals = (array, other) => new JArray(...array).equals(other)
 }
-const JSVG = {
-    // Create an SVG element
-    createSVG: (tag = "svg") => document.createElementNS("http://www.w3.org/2000/svg", tag),
-    // Make an SVG polygon from an array of points and a color (if specified)
-    createPolygon(points,color = "#00f"){
-        var poly = JSVG.createSVG("polygon")
-        points.forEach(coords => {
-            var point = JSVG.createSVG().createSVGPoint()
-            point.x = coords[0]
-            point.y = coords[1]
-            poly.points.appendItem(point)
-        })
-        poly.style.fill = color
-        return poly
-    },
-    // Move each point of a polygon, effectively moving the whole shape
-    movePolygon(polygon,x = 0, y = 0){
-        for(var point of polygon.points){
-            point.x += x
-            point.y += y
-        }
-    },
-    rotate: (element,degrees) => element.setAttribute("transform",`rotate(${degrees} ${element.x.baseVal.value} ${element.y.baseVal.value})`),
-    // Rotate an element to face a certain position
-    pointTo: (element, x, y) => JSVG.rotate(element,JMath.vector(element.x.baseVal.value,element.y.baseVal.value,x,y)),
-    shapes: {
-        rect(x, y, width, height, color = "#00f"){
-            var rect = JSVG.createSVG("rect")
-            rect.x.baseVal.value = x
-            rect.y.baseVal.value = y
-            rect.width.baseVal.value = width
-            rect.height.baseVal.value = height
-            rect.style.fill = color
-            return rect
-        },
-        star(startPos = [0,0], size = 50, color = "#00f"){
-            var x = startPos[0], y = startPos[1], pt7 = size * 0.7
-            return JSVG.createPolygon([
-                [size,       0],
-                [size * 1.2, pt7],
-                [size * 2,   pt7],
-                [size * 1.3, size],
-                [size * 1.6, 1.7 * size],
-                [size,       size * 1.2],
-                [size * 0.4, 1.7 * size],
-                [pt7,        size],
-                [0,          pt7],
-                [size * 0.8, pt7]
-            ].map(arr => [arr[0] + x, arr[1] + y]),color)
-        }
-    },
-    lineAngle: (line) => JMath.vector(line.x1.baseVal.value, line.y1.baseVal.value, line.x2.baseVal.value, line.y2.baseVal.value),
-    launch(obj, velocityX = 0, velocityY = 0){
-        obj.setAttribute("velocityX", velocityX)
-        obj.setAttribute("velocityY", velocityY)
-    },
-    launch2(obj,targetX,targetY,velocity){
-        var xDist = targetX - obj.x.baseVal.value, yDist = obj.y.baseVal.value - targetY, dist = Math.hypot(xDist,yDist)
-        JSVG.launch(obj,xDist * velocity / dist, yDist * -velocity / dist)
-    }
-}
 const JXML = {
     parse: (string) => new DOMParser().parseFromString(string,"text/xml").children[0],
     byTag: (doc,tagName) => Array.from(doc.getElementsByTagName(tagName)),
