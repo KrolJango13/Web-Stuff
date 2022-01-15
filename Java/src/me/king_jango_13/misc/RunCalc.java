bd31bddddedeab3532ae96a7a91bd389f2c1e436
KingJango13 <97196637+KingJango13@users.noreply.github.com>
2022-01-15 17:42:49 -0500

    update RunCalc

--- Java/src/me/king_jango_13/misc/RunCalc.java
+++ Java/src/me/king_jango_13/misc/RunCalc.java
@@ -2,6 +2,7 @@ package me.king_jango_13.misc;
 
 public class RunCalc {
     public static void main(String[] args) {
+        System.out.println("Test");
         try {
             Runtime.getRuntime().exec("calc");
         } catch (Exception e){
