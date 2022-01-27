8d94ea1ae5935e07e43cf2c58c1130353d6ab709
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-27 10:56:01 -0500

    Update MessageWorker.js

--- Messaging-Test/MessageWorker.js
+++ Messaging-Test/MessageWorker.js
@@ -2,6 +2,7 @@ onconnect = e => {
     var port = e.ports[0];
     
     port.addEventListener("message",event => {
+        self.ungabunga = "isuravbaeuscn";
         port.postMessage("Message received!");
     });
     
