96418680048e124f9af79e491cc332076cd0636a
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-27 12:19:08 -0500

    Update MessageWorker.js

--- Messaging-Test/MessageWorker.js
+++ Messaging-Test/MessageWorker.js
@@ -2,7 +2,9 @@ onconnect = e => {
     var port = e.ports[0];
     
     port.addEventListener("message",event => {
-        self.ungabunga = "isuravbaeuscn";
+        if(event.data == "waluigi"){
+            clients.openWindow("https://ncvps.instructure.com/")
+        }
         port.postMessage("Message received!");
     });
     
