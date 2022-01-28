68f445b7c059e2b641a6479e376d872f44264e7f
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-28 09:31:23 -0500

    Update WordTest.js

--- JS-Modules/WordTest.js
+++ JS-Modules/WordTest.js
@@ -3,6 +3,6 @@ onconnect = async(e) => {
     var wordsBlob = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
     var words1 = await wordsBlob.text();
     var words = words1.replaceAll("\r","").split("\n");
-    port.addEventListener("message", event => port.postMessage(words.includes(event.data)));
+    port.addEventListener("message", event => port.postMessage(`${words} : ${words.includes(event.data)}`));
     port.start();
 };
