32fffc0eff439e965a9713a3d722cc26e1513b97
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-28 10:06:54 -0500

    Create JangoWorker.js

---
+++ JangoWorker.js
@@ -0,0 +1,2 @@
+const jangoModule = (name) => `https://kroljango13.github.io/Web-Stuff/JS-Modules/${name}.js`;
+importScripts(jangoModule("JMath"),jangoModule("JXML"),jangoModule("JSVG"),jangoModule("JArray"));
