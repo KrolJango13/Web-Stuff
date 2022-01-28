b2a8583ce698c4e20ee9dc69f70d49df9ad0eb49
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-28 11:09:28 -0500

    Update JangoWorker.js

--- JangoWorker.js
+++ JangoWorker.js
@@ -1,3 +1,2 @@
 const jangoModule = (name) => `https://kroljango13.github.io/Web-Stuff/JS-Modules/${name}.js`;
-importScripts(jangoModule("JRandom"));
-//jangoModule("JMath"),jangoModule("JXML"),jangoModule("JSVG"),jangoModule("JArray")
+importScripts(jangoModule("JMath"), jangoModule("JXML"), jangoModule("JSVG"), jangoModule("JArray"), jangoModule("JRandom"));
