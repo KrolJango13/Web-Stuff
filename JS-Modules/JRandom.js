75f628aa2e5b71f26edb6b968de7b2b9c414205c
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-31 12:38:20 -0500

    Update JRandom.js

--- JS-Modules/JRandom.js
+++ JS-Modules/JRandom.js
@@ -1,3 +1,13 @@
-const JRandom = {
-    int: (xBound) => Math.floor(Math.random() * xBound)
+const int = (xBound) => Math.floor(Math.random() * xBound);
+const rangedInt(min,xMax) => int(xMax - min) + min;
+const arrayMember = (array) => array[int(array.length)];
+const char = (codeRange = {min:0x41,max:0xff}) => String.fromCodePoint(rangedInt(codeRange.min,codeRange.max + 1));
+const string = (length,codeRange = {min:0x41,max:0xff}) => new Array(length).fill(char(codeRange)).join("");
+async function word(){
+    var wordBlob = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+    var wordsTxt = await wordBlob.text();
+    var words = wordsTxt.replaceAll("\r","").split("\n");
+    return arrayMember(words);
 }
+
+export {int,rangedInt,arrayMember,char,string,word}
