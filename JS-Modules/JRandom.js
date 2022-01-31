58e32de56aa9925bab89f97a64be2cf1e607a1cd
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-31 14:44:30 -0500

    Update JRandom.js

--- JS-Modules/JRandom.js
+++ JS-Modules/JRandom.js
@@ -1,12 +1,11 @@
+var wordsPromise = fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt").then(x => x.text()).then(x => x.replaceAll("\r","").split("\n"));
 const int = (xBound) => Math.floor(Math.random() * xBound);
 const rangedInt = (min,xMax) => int(xMax - min) + min;
 const arrayMember = (array) => array[int(array.length)];
 const char = (codeRange = {min:0x41,max:0xff}) => String.fromCodePoint(rangedInt(codeRange.min,codeRange.max + 1));
 const string = (length,codeRange = {min:0x41,max:0xff}) => new Array(length).fill(char(codeRange)).join("");
 async function word(){
-    var wordBlob = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
-    var wordsTxt = await wordBlob.text();
-    var words = wordsTxt.replaceAll("\r","").split("\n");
+    var words = await wordsPromise;
     return arrayMember(words);
 }
 
