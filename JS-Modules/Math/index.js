1d5c8620b21865319585e2576b89c37f6ad6f88d
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-01-31 10:59:27 -0500

    Update index.js

--- JS-Modules/Math/index.js
+++ JS-Modules/Math/index.js
@@ -27,5 +27,5 @@ const pi = {
     str: "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
 }
 
-export {sqrt,range,range0,factorial,factors,isPrime,gcf,lcm,pi,Geometry};
+export {sqrt,range,range0,factorial,factors,isPrime,gcf,lcm,pi};
 export * from "./Geometry.js" 
