b7302f8b9257fb164401702957b5b652f1fd0276
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-02-02 15:12:17 -0500

    Update JangoWorker.js

--- JangoWorker.js
+++ JangoWorker.js
@@ -1,9 +1,13 @@
-let channels = {}
 onmessage = async(e) => {
     var userCache = await caches.open("users")
     userCache.put(e.origin,new Response(e.origin.split("#")[1]))
     userCache.match(`${e.origin.split("#")[0]}#{e.data.to}`).then(user => {
-        var channel = new BroadcastChannel(e.data.to);
-        channel.postMessage({sender:e.origin.split("#")[1],msg:e.data.msg})
-    }).catch(err => e.source.postMessage({sender:"Worker",msg:"Couldn't find user " + e.data.to}))
+        if(user){
+            var channel = new BroadcastChannel(e.data.to);
+            channel.postMessage({sender:e.data.sender,msg:e.data.msg})
+        } else {
+            var channel = new BroadcastChannel(e.data.to);
+            channel.postMessage({sender:"Worker",msg:"Couldn't find user " + e.data.to})
+        }
+    })
 }
