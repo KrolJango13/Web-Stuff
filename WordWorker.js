bf5b4e41ea606367e664e3c211ac7c650944b1ec
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-02-08 12:01:27 -0500

    Create WordWorker.js

---
+++ WordWorker.js
@@ -0,0 +1,37 @@
+oninstall = e => {
+    e.waitUntil(
+        fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt")
+        .then(x => x.text())
+        .then(x => self.words = x.replaceAll("\r","").split("\n"))
+    )
+}
+
+onmessage = e => {
+    const reply = e.source.postMessage
+    switch(e.data.method){
+        case "random":{
+            if(e.data.msgObj.length){
+                reply(self.words.filter(x => x.length === e.data.msgObj.length)[Math.floor(Math.random() * self.words.length)]);
+            } else {
+                reply(self.words[Math.floor(Math.random() * self.words.length)]);
+            }
+            break;
+        }
+        case "test":{
+            if(e.data.msgObj.word){
+                reply(self.words.includes(e.data.msgObj.word));
+            } else {
+                reply("Specify a word property on msgObj")
+            }
+            break;
+        }
+        case "help": {
+            reply("Methods:\n\random,\n\ttest,")
+            break;
+        }
+        default: {
+            reply("Send message as {method: (a method, help if you need help), msgObj: (an object with specified params)}")
+            break;
+        }
+    }
+}
