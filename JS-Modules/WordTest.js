83a5ec351f639f0fbd8c56aec2b46f63087a0751
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-02-08 11:41:35 -0500

    Delete WordTest.js

--- JS-Modules/WordTest.js
+++
@@ -1,8 +0,0 @@
-onconnect = async(e) => {
-    var port = e.ports[0];
-    var wordsBlob = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
-    var words1 = await wordsBlob.text();
-    var words = words1.replaceAll("\r","").split("\n");
-    port.addEventListener("message", event => port.postMessage(words.includes(event.data)));
-    port.start();
-};
