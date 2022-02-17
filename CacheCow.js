dcd9ec91bbaebc08cc440cc8eb2508b3d0b4b860
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-02-17 12:33:50 -0500

    Update and rename CacheCow.js to CharWorker.js

--- CacheCow.js
+++
@@ -1,8 +0,0 @@
-let cashCache;
-oninstall = async(e) => {
-    cashCache = await caches.open("CashCache");
-    cashCache.add("https://kroljango13.github.io/Web-Stuff/Character%20Codes/MathChars.json")
-}
-onmessage = async(e) => {
-    e.ports[0].postMessage(await cashCache.match("https://kroljango13.github.io/Web-Stuff/Character%20Codes/MathChars.json"))
-}
