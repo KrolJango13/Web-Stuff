803aeec961ae9859d246f08f7adc4d54ae043540
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-02-17 12:18:38 -0500

    Update CacheCow.js

--- CacheCow.js
+++ CacheCow.js
@@ -1,8 +1,8 @@
-let cashCache, $URL = "https://www.google.com/search?q=dollar+sign&safe=active&rlz=1CATAVM_enUS946US946&tbm=isch&source=iu&ictx=1&vet=1&fir=3owrVvPYA5cElM%252CdbhFl9YvSZ6unM%252C_%253BtKd1qCSW0qQ2PM%252C8V5i1WaLNa7BrM%252C_%253BrZNelyarPXTp2M%252CesM5uezMDnqgzM%252C_&usg=AI4_-kTE9kuu-wYA4BX_YHx58dvbkVwjkg&sa=X&ved=2ahUKEwjnxPvxnIf2AhWGdt8KHTYhAf4Q_h16BAgEEAE#imgrc=3owrVvPYA5cElM";
+let cashCache;
 oninstall = async(e) => {
     cashCache = await caches.open("CashCache");
-    cashCache.add($URL)
+    cashCache.add("https://kroljango13.github.io/Web-Stuff/Character%20Codes/MathChars.json")
 }
 onmessage = async(e) => {
-    e.ports[0].postMessage(await cashCache.match($URL))
+    e.ports[0].postMessage(await cashCache.match("https://kroljango13.github.io/Web-Stuff/Character%20Codes/MathChars.json"))
 }
