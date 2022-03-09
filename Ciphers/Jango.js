419748ce11ae1c4dac4a36332a9a22adad2d71a2
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-03-09 10:20:20 -0500

    Update Jango.js

--- Ciphers/Jango.js
+++ Ciphers/Jango.js
@@ -2,15 +2,15 @@
 A cipher I made that returns an obfuscated string that varies based on the time created
 Each string can have 1,440 different results! Quite a pain to do by hand!
 */
-String.prototype.jangoEncode = function(date = new Date()){
-    var h = date.getHours(), m = date.getMinutes()
-    var str = `At ${h}:${m}\n`;
-    for(var char of this){
-        str += String.fromCodePoint(char.codePointAt() ^ (h * 60) + m)
-    }
-    return str
+function charXOR(str,h,m){
+    return str.split("").map(c => String.fromCodePoint(c.codePointAt() ^ ((h * 60) + m))).join("");
 }
 
+String.prototype.jangoEncode = function(date = new Date()){
+    var h = date.getHours(), m = date.getMinutes();
+    return `[At ${h}:${m}]:    ${charXOR(this,h,m)}`;
+};
+
 String.prototype.jangoDecode = function(hours, minutes){
-    return this.split("").map(x => String.fromCodePoint(x.codePointAt() ^ ((hours * 60) + m))).join("");
-}
+    return charXOR(this,hours,minutes);
+};
