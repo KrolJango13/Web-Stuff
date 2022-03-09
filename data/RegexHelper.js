b58bf0931454ef1df6de7db9c8e995caff4e0a67
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-03-09 14:55:41 -0500

    Create RegexHelper.js

---
+++ data/RegexHelper.js
@@ -0,0 +1,54 @@
+const ImportantRGX = {
+    oneLineComment: /\/\/.*$/g,
+    multiLineComment: /\/\*.*\n*(.*\n)*\n*\*\//g,
+    func: (name) => new RegExp(`(^|\\0)\\s*${name}\s?\\((.*)\\)`)
+}
+
+function stringIndexes(str,quoteType = '"'){
+    var isStr = false, strIndexes = [];
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === quoteType){
+            isStr = !isStr;
+            continue;
+        }
+        if(isStr){
+            strIndexes.push(i);
+        }
+    }
+    return strIndexes;
+}
+
+function comment1LIndexes(str){
+    var prev = "", isComm = false, indexes = [];
+    for(var i = 0; i < str.length; i++){
+        if(prev + str[i] === "//"){
+            isComm = true;
+            continue;
+        } else if(str[i] === "\n"){
+            isComm = false;
+        }
+        if(isComm){
+            indexes.push(i);
+        }
+        prev = str[i]
+    }
+    return 
+}
+
+
+function commentMultiLIndexes(str){
+    var prev = "", isComm = false, indexes = [];
+    for(var i = 0; i < str.length; i++){
+        if(prev + str[i] === "/*"){
+            isComm = true;
+            continue;
+        } else if(prev + str[i] === "*/"){
+            isComm = false;
+        }
+        if(isComm){
+            indexes.push(i);
+        }
+        prev = str[i]
+    }
+    return 
+}
