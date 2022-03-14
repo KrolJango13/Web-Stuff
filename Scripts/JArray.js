10bb3a95b785f7be87ff2af21ffdb627aa3aa7ee
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-03-14 12:26:33 -0400

    Update JArray.js

--- Scripts/JArray.js
+++ Scripts/JArray.js
@@ -1,34 +1,63 @@
 let arrProto = Array.prototype;
+const {floor,random} = Math;
 
 arrProto.middle = function(){
     var half = this.length / 2;
     if(this.length % 2){
-        return this[Math.floor(half)]
+        return this[floor(half)]
     }
     return [this[half-1],this[half]]
 }
 
-arrProto.max = function(){return this.sort().pop()}
-arrProto.min = function(){return this.sort()[0]}
+arrProto.last = function(){
+    return [...this].pop();
+}
+
+arrProto.max = function(){
+    return this.sort((x,y) => y - x)[0];
+}
+arrProto.min = function(){
+    return this.sort((x,y) => x - y)[0];
+}
     
 arrProto.divide = function(size){
-    var arr = new Array(Math.ceil(this.length / size)).fill(new Array(size))
-    for(var i = 0; i < arr.length; i++){
-        var arr1 = []
-        for(var j = 0; j < size; j++){
-            arr1.push(this[(i * size) + j])
-        }
-        arr[i] = arr1
+    var arr = [];
+    for(var i = 0; i < arr.length; i+=size){
+        arr.push(this.slice(i,i + size));
     }
     return arr;
 }
 
 arrProto.randomMember = function(){
-    return this[Math.floor(Math.random() * this.length)];
+    return this[floor(random() * this.length)];
 };
 
 arrProto.forEach1 = function(func){
     for(var x of this){
         func(x);
     }
+}
+
+arrProto.cycleLeft = function(amount){
+    for(var i = 0; i < amount; i++){
+        this.push(this.shift());
+    }
+}
+
+arrProto.cycleRight = function(amount){
+    for(var i = 0; i < amount; i++){
+        this.unshift(this.pop());
+    }
+}
+
+arrProto.cycledLeft = function(amount){
+    var arr = [...this];
+    arr.cycleLeft(amount);
+    return arr;
+}
+
+arrProto.cycledRight = function(amount){
+    var arr = [...this];
+    arr.cycleRight(amount);
+    return arr;
 }
