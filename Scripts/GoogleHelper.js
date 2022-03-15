69c760db776c468eaeac79505e0055df1041771f
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-03-15 11:03:22 -0400

    Update GoogleHelper.js

--- Scripts/GoogleHelper.js
+++ Scripts/GoogleHelper.js
@@ -2,12 +2,12 @@
 window.JangoFormHelper = {
     // Get the fields of the form you are on
     getFormEntries(){
-        var fields = [...document.querySelector("form > div > div").children]
-        return fields.map(x => Object.fromEntries([[x.name,x.value]])).reduce((x,y) => Object.fromEntries(y,x));
+        var fields = [...document.querySelector("form > div > div").children];
+        return fields.map(x => Object.fromEntries([[x.name,x.value]])).reduce((x,y) => Object.assign(y,x));
     },
     
     // Submit a form with the specified key
-    submitToForm(formKey,entries) => fetch(`https://docs.google.com/forms/u/0/d/e/${formKey}/formResponse?${new URLSearchParams(entries).toString()}&submit=Submit`),
+    submitToForm: (formKey,entries) => fetch(`https://docs.google.com/forms/u/0/d/e/${formKey}/formResponse?${new URLSearchParams(entries).toString()}&submit=Submit`),
     
     // Get the key of the form you are on
     getFormKey: () => location.pathname.split("/")[4],
