9abc140b992c8cecce636922e062e569f8bea257
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-03-15 10:21:12 -0400

    Update JString.js

--- Scripts/JString.js
+++ Scripts/JString.js
@@ -42,4 +42,80 @@
         return strCheck.call(this,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,. ")
     };
     
+    strProto.modCodePoint = function(func,index = 0){
+        return String.fromCodePoint(func(this.codePointAt(index)))
+    };
+    strProto.shiftCodePoint = function(amount,index = 0){
+        return this.modCodePoint(x => x + amount, index);
+    };
+    
+    strProto.forEach = function(func){
+        for(var i = 0; i < this.length; i++){
+            func(this[i],i,this);
+        }
+    };
+    strProto.forEach1 = function(func){
+        for(var char of this){
+            func(char);
+        }
+    };
+    
+    strProto.map = function(func){
+        var str = "";
+        for(var char of this){
+            str += func(char);
+        }
+        return str;
+    };
+    
+    strProto.some = function(predicate){
+        for(var char of this){
+            if(predicate(char)){
+                return true;
+            }
+        }
+        return false;
+    };
+    
+    strProto.every = function(predicate){
+        for(var char of this){
+            if(!predicate(char)){
+                return false;
+            }
+        }
+        return true;
+    };
+    
+    strProto.shiftAlphabet = function(upperShift){
+        if(this.isUpperCase()){
+            return this.shiftCodePoint(upperShift);
+        }
+        return this.shiftCodePoint(upperShift - 32);
+    }
+    
+    strProto.toRegionChar = function(){
+        return this.shiftAlphabet(0x1f1a5);
+    }
+    
+    strProto.circleLetters = function(){
+        if(this.isUpperCase()){
+            return this.shiftCodePoint(0x2475);
+        }
+        return this.shiftCodePoint(0x246f);
+    };
+    
+    strProto.unicodeAt = function(index = 0){
+        return this.codePointAt(index);
+    };
+    
+    strProto.getBytes = function(){
+        var hex = this.toHexCodes().join(""), bytes = [];
+        if(hex.length % 2){
+            hex = "0" + hex;
+        }
+        for(var i = 0; i < hex.length; i+=2){
+            bytes.push(parseInt(hex.substr(i,2),16));
+        }
+        return bytes;
+    };
 })();
