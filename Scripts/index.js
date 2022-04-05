5716405f2b1e7f03c843b0045680a01592b595d7
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-04-05 09:21:02 -0400

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,18 +2,30 @@ const getScript = (name,path = "Scripts") => import(`https://kroljango13.github.
     .then(x => console.log(`Successfully imported ${name}.js from ${path}`))
     .catch(err => console.error(`Failed to import ${name}.js from ${path} because of:\n${err}`))
 
-getScript("JArray");
-getScript("JString");
-getScript("JObjects");
-getScript("JNumber");
-getScript("Time");
-getScript("RegexHelper");
-getScript("GoogleHelper");
+const scripts = {
+    Scripts: [
+        "JArray",
+        "JString",
+        "JObjects",
+        "JNumber",
+        "RegexHelper",
+        "GoogleHelper",
+        "HTMLHelper"
+    ],
+    Science: [
+        "ElectronConfig",
+        "Light"
+    ],
+    Ciphers: [
+        "Arrows",
+        "Caesar",
+        "Jango",
+        "Vigenere"
+    ]
+};
 
-getScript("ElectronConfig","Science");
-getScript("Light","Science");
-
-getScript("Arrows","Ciphers");
-getScript("Caesar","Ciphers");
-getScript("Jango","Ciphers");
-getScript("Vigenere","Ciphers");
+for(var path of Object.keys(scripts)){
+    for(var script of scripts[path]){
+        getScript(script,path);
+    }
+}
