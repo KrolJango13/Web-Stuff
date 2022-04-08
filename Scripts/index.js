496df94b8c52e3d8b17032a33a793527d836f9cd
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-04-08 11:17:36 -0400

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,31 +1,30 @@
-const getScript = (name,path = "Scripts") => import(`https://kroljango13.github.io/Web-Stuff/${path}/${name}.js`)
+const WEB_STUFF = "https://kroljango13.github.io/Web-Stuff";
+
+const getScript = (name,path = "Scripts") => import(`${WEB_STUFF}/${path}/${name}.js`)
     .then(x => console.log(`Successfully imported ${name}.js from ${path}`))
-    .catch(err => console.error(`Failed to import ${name}.js from ${path} because of:\n${err}`))
+    .catch(err => console.error(`Failed to import ${name}.js from ${path} because of:\n${err}`));
 
-const scripts = {
-    Scripts: [
-        "JArray",
-        "JString",
-        "JObjects",
-        "JNumber",
-        "RegexHelper",
-        "GoogleHelper",
-        "HTMLHelper"
-    ],
-    Science: [
-        "ElectronConfig",
-        "Light"
-    ],
-    Ciphers: [
-        "Arrows",
-        "Caesar",
-        "Jango",
-        "Vigenere"
-    ]
-};
-
-for(var path of Object.keys(scripts)){
-    for(var script of scripts[path]){
-        getScript(script,path);
-    }
+function getAllScripts(path){
+    console.log(`Getting all scripts from ${path}...`);
+    import(`${WEB_STUFF}/${path}/index.js`)
+        .then(x => console.log(`Successfully imported all scripts from ${path}`))
+        .catch(err => console.error(`Failed to load scripts from ${path} because of:\n${err}`));
 }
+
+const scripts = [
+    "JArray",
+    "JString",
+    "JObjects",
+    "JNumber",
+    "RegexHelper",
+    "GoogleHelper",
+    "HTMLHelper"
+];
+
+(async function loadAll(){
+    for(var script of scripts){
+        await getScript(script);
+    }
+    await getAllScripts("Science");
+    await getAllScripts("Ciphers");
+})();
