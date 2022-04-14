a3be1e7abde3568a7e4cca03569abbd5b9fccf30
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-04-14 11:15:51 -0400

    Update ChemEquations.js

--- Science/ChemEquations.js
+++ Science/ChemEquations.js
@@ -1,63 +1,12 @@
-/*
-function getElems(...molecules){
-    var elems = [];
-    for(var molec of molecules){
-        for(var elem of Object.keys(molec)){
-            if(!elems.includes(elem.toLowerCase())){
-                elems.push(elem.toLowerCase());
-            }
-        }
-    }
-    return elems;
+WebAssembly.instantiateStreaming(fetch("https://kroljango13.github.io/Web-Stuff/Science/ChemEquations.wasm"))
+    .then(x => {
+        window.balanceChem = function(elements, maxCoef = 20){
+            var l = elements.length;
+            var arr = new Int32Array(x.instance.exports.memory.buffer, 0, l);
+            arr.set(elements);
+            var int = x.instance.exports.base(l, arr, maxCoef);
+            return [int >> 24, ((int >> 16) % 256), ((int >> 8) % 256), int % 256];
+        };
+    });
 }
 
-function EGetter(elem){
-    return (x,y) => (x[elem] || 0) * y;
-}
-
-function getCoefs(a,b,c,d,maxCoef = 20){
-    var elems = getElems(a,b,c,d);
-    for(var i = 1; i < maxCoef + 1; i++){
-        for(var j = 1; j < maxCoef + 1; j++){
-            for(var k = 1; k < maxCoef + 1; k++){
-                for(var l = 1; l < maxCoef + 1; l++){                
-                    if(elems.every((elem) => {
-                        const e = EGetter(elem);
-                        return e(a,i) + e(b,j) == e(c,k) + e(d,l);
-                    }))return [i, j, k, l];
-                }
-            }
-        }
-    }
-}
-
-function getCoefsSynthesis(a,b,product,maxCoef = 20){
-    var elems = getElems(a,b,product);
-    for(var i = 1; i < maxCoef + 1; i++){
-        for(var j = 1; j < maxCoef + 1; j++){
-            for(var k = 1; k < maxCoef + 1; k++){
-                if(elems.every((elem) => {
-                    const e = EGetter(elem);
-                    return e(a,i) + e(b,j) == e(product,k);
-                }))return [i, j, k];
-            }
-        }
-    }
-}
-
-function getCoefsDecomp(reactant,c,d,maxCoef = 20){
-    var synth = getCoefsSynthesis(c,d,reactant,maxCoef);
-    synth.unshift(synth.pop());
-    return synth;
-}
-*/
-
-window.ChemBalancer = {};
-WebAssembly.instantiateStreaming(fetch("https://kroljango13.github.io/Web-Stuff/Science/ChemEquations.wasm")).then(x => x.instance.exports).then(wasmMod => {
-    
-    ChemBalancer.base = function(elements,maxCoef = 20){
-        var arr = new Int32Array(wasmMod.memory.buffer,0,elements.length);
-        arr.set(elements);
-        return wasmMod.base(elements.length,arr,maxCoef)
-    }
-})
