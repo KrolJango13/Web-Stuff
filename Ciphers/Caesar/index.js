30d3e42b1afc802d5345a3a9b7d5c2878fc7003b
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2022-05-13 11:55:11 -0400

    Update index.js

--- Ciphers/Caesar/index.js
+++ Ciphers/Caesar/index.js
@@ -1,12 +1,13 @@
-var wasmB64 = "AGFzbQEAAAABh4CAgAABYAJ/fwF/A4KAgIAAAQAEhICAgAABcAAABYOAgIAAAQABBoGAgIAAAAeZgICAAAIGbWV" +
-    "tb3J5AgAMY2Flc2FyQ2lwaGVyAAAK9ICAgAAB7oCAgAAAAkAgAEG/f2pB/wFxQTlLDQAgAEGlf2pB/wFxQQZJDQAgAUEab0Ea" +
-    "akEabyIBQQFIDQADQAJAIABBIHJB/wFxQfoARw0AIABB/wFxQeYBaiEACyAAQQFqIQAgAUF/aiIBDQALCyAAQRh0QRh1Cw==";
+var wasmB64 = "AGFzbQEAAAABh4CAgAABYAJ/fwF/A4KAgIAAAQAEhICAgAABcAAABYOAgIAAAQABBoGAgIAAAAeagICAAAIGbWV\
+tb3J5AgANY2Flc2FyX2xldHRlcgAACuOAgIAAAd2AgIAAAQF/AkAgAEG/f2pB/wFxQTlLDQBBwQBBACAAQdsASBtB4QBBACAAQeAAS\
+htqIgJFDQAgAUEab0EaakEabyAAaiACQRh0QRh1IgBrQRpvIABqIQALIABBGHRBGHUL";
+var wasmBuff = Uint8Array.from(atob(wasmB64).split("").map(x => x.codePointAt()));
+var wasmCaesar = new WebAssembly.Instance(new WebAssembly.Module(wasmBuff)).exports.caesar_letter;
 
-async function load(){
-    var wasmMod = await WebAssembly.instantiateStreaming(fetch("data:application/wasm;base64," + wasmB64));
-    
-    window.caesarCipher = function(text,shift){
-        return text.split("").map(x => String.fromCodePoint(wasmMod.instance.exports.caesarCipher(x,shift))).join("");
+window.caesarCipher = function(text,shift){
+    var coded = "";
+    for(var char of text){
+        coded += String.fromCodePoint(wasmCaesar(char.codePointAt(),shift));
     }
+    return coded;
 }
-load();
