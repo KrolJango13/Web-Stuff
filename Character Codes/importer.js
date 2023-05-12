2df4c74c36a6f8f419635b2510af4b181511469c
KrolJango13 <97181369+KrolJango13@users.noreply.github.com>
2023-05-12 11:12:48 -0400

    Update importer.js

--- Character Codes/importer.js
+++ Character Codes/importer.js
@@ -1,8 +1,15 @@
 const getCharset = (name) => fetch(`https://kroljango13.github.io/Web-Stuff/Character%20Codes/${name}.json`).then(x => x.json());
 const JangosChars = [];
+
 function extractChars(obj){
     for(var {char,code} of Object.values(obj)){
         JangosChars.push([char,code]);
+    }
+}
+
+function extractCharList(obj){
+    for(var char of Object.entries(obj)){
+        JangoChars.push(char);
     }
 }
 
@@ -13,6 +20,7 @@ function extractAlphabet(alphabet){
 }
 
 (async() => {
+    extractCharList(await getCharset("LatinExtended"));
     var {symbols,fractions,superscripts,subscripts} = await getCharset("MathChars");
     var {one_numerator,half,third,fourth,fifth,sixth,eighth} = fractions
     
@@ -29,9 +37,7 @@ function extractAlphabet(alphabet){
     extractAlphabet(await getCharset("PolishChars"));
     extractAlphabet(await getCharset("RussianChars"));
     
-    for (var char of Object.entries(await getCharset("Chars"))){
-        JangosChars.push(char);
-    }
+    extractCharList(await getCharset("Chars"))
     
     window.JangosChars = JangosChars;
 })();
